b47670dd033a59ca96239cb8d69ae3edd4434272
ronalr2k <ronalr2k@gmail.com>
2016-08-29 18:26:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# finalCRUDlaravel5.2
+# finalCRUDlaravel5.2
---
+++ resources/views/bodyFrond.blade.php
@@ -0,0 +1,160 @@
+@extends('layouts.masterFrond')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-logo" style="background-image: url({{asset("img/back.jpg")}})">
+                <div class="slogan col-md-3">
+                    <h4>HERE YOU CAN TYPE</h4>
+                    <h4>YOUR COMPANY</h4>
+                    <h4>slogan</h4>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-12 "></div>
+    </div>
+    <div class="content">
+        <h1><strong>Create Your Own Genuine Web Masterpiece</strong></h1>
+        <h5 class="hidden col-sm col-xs"> loren ipsum is dummy text of the princing and type setting industry.</h5>
+    </div
+
+    <div class="wrapper">
+        <div class="triangle-down"><div></div></div>
+    </div>
+
+
+    <div class="filaCuatro container-fluid">
+        <div class="container">
+            <div class="col-md-4 text-center">
+                <img src="{{asset('img/foco.png')}}">
+                <h2><strong>Ideas</strong></h2>
+                <p>
+                    A continuación, debes editar el archivo Homestead.yaml. En este archivo, puedes configurar la ruta de tu SSH Key pública, así como las carpetas que quieres compartir entre tu máquina principal y la máquina virtual Homestead.
+                </p>
+            </div>
+            <div class="col-md-4 text-center img-responsive">
+                <img src="{{asset('img/mundosearch.png')}}">
+                <h2><strong>Make research</strong></h2>
+                <p>
+                    A continuación, debes editar el archivo Homestead.yaml. En este archivo, puedes configurar la ruta de tu SSH Key pública, así como las carpetas que quieres compartir entre tu máquina principal y la máquina virtual Homestead.
+                </p>
+            </div>
+            <div class="col-md-4 text-center">
+                <img src="{{asset('img/set.png')}}">
+                <h2><strong> Do the work</strong></h2>
+                <p>
+                    A continuación, debes editar el archivo Homestead.yaml. En este archivo, puedes configurar la ruta de tu SSH Key pública, así como las carpetas que quieres compartir entre tu máquina principal y la máquina virtual Homestead.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=" carusel container-fluid">
+
+    </div>
+    <div class="wrapper fondYellowa">
+        <div class="fondYellowa triangle-down"><div></div></div>
+    </div>
+    <div class="container-fluid fondcont">
+        <div  class="container ">
+            <div class="row text-center">
+                <h2><strong>Some of Our Work</strong></h2>
+                <h5> loren imput simple dammy text of the  printing and typesettting industrie</h5>
+            </div>
+            <div class="container-fluid text-center">
+                <div class="col-md-2">
+
+                </div>
+                <div class="col-md-9 text-center">
+                    <img class=" img img-responsive" src="img/carousel.jpg">
+                </div>
+                <div class="col-md-1">
+
+                </div>
+             </div>
+        </div>
+    </div>
+    </div
+
+    <div class="wrapper fondYellowb">
+        <div class=" fondYellowb triangle-down"><div></div></div>
+    </div
+    </div>
+    <div class="container-fluid">
+        <div class="row text-center">
+            <h1><strong>Our Last Story</strong></h1>
+            <h5>loren imput dummy text of the print typeseeting industry</h5>
+            <br>
+            <br>
+        </div>
+    </div>
+    <!--news-->
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-4">
+                <img src="img/news.png">
+            </div>
+            <div class="col-md-4">
+                <img src="img/news.png">
+            </div>
+            <div class="col-md-4">
+                <img src="img/news.png">
+            </div>
+        </div>
+    <!-- end news -->
+
+    </div>
+    <div class="  triWhite container-fluid">
+    </div
+    <div class="wrapper">
+        <div class="fondYellow triangle-down"><div></div></div>
+    </div>
+    </div>
+    <div class="  container-fluid meter">
+        <div>
+            <div class="row text-center">
+                <div class="container text-center">
+                    <div class="col-md-1">
+
+                    </div>
+                    <div class="col-md-2 col-xs-12 ">
+                        <div>
+                            <img src="img/footer.jpg">
+                        </div>
+                        <p>InDesing</p>
+                    </div>
+                    <div class="col-md-2 col-xs-12 ">
+                        <div>
+                            <img src="img/footer.jpg">
+                        </div>
+                        <p>AfterEfects</p>
+                    </div>
+                    <div class="col-md-2 col-xs-12 ">
+                        <div>
+                            <img src="img/footer.jpg">
+                        </div>
+                        <p>Illustrator</p>
+                    </div>
+                    <div class="col-md-2 col-xs-12 ">
+                        <div>
+                            <img src="img/footer.jpg">
+                        </div>
+                        <p>Photoshop</p>
+                    </div>
+                    <div class="col-md-2 col-xs-12 ">
+                        <div>
+                            <img src="img/footer.jpg">
+                        </div>
+                        <p>Premiere</p>
+                    </div>
+                    <div class="col-md-1">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+@endsection
